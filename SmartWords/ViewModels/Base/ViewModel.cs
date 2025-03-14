ba09bb20f7ca4fda209469d1532b8c24ec9a8e30
molodoi1337 <nniki@DESKTOP-8AHT603>
2025-03-14 17:59:48 +0300

    Добавил Возможность создавать команды(Command and LambdaCommand)

--- SmartWords/ViewModels/Base/ViewModel.cs
+++ SmartWords/ViewModels/Base/ViewModel.cs
@@ -5,14 +5,14 @@ namespace SmartWords.ViewModels.Base
 {
     internal abstract class ViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
 
-        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = "")
         {
             if (Equals(field, value)) return false;
             field = value;
--- SmartWords/ViewModels/MainWindowViewModel.cs
+++ SmartWords/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,36 @@
-﻿using SmartWords.ViewModels.Base;
+﻿using SmartWords.Infrastructure.Commands.Base;
+using SmartWords.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
 
 namespace SmartWords.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Команды
+
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+            
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
